765520c14b4999256673852791f7f4f0c5343410
David <david@taiko.xyz>
2025-01-10 08:49:47 +0800

    Revert "chore(cmd): remove `--taiko.preconfirmationForwardingUrl` flag (#362)"
    This reverts commit 283fedd05b57bbedc8142601ac86e9992b3c12cd.

--- eth/ethconfig/config.go
+++ eth/ethconfig/config.go
@@ -157,6 +157,9 @@ type Config struct {
 
 	// OverrideVerkle (TODO: remove after the fork)
 	OverrideVerkle *uint64 `toml:",omitempty"`
+
+	// CHANGE(taiko): add preconfirmation forwarding URL
+	PreconfirmationForwardingURL string
 }
 
 // CreateConsensusEngine creates a consensus engine for the given chain config.
