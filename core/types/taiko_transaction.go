4dd1989387372f3ffe37ae70b47774ce587b0a29
David <david@taiko.xyz>
2025-01-23 23:02:29 +0800

    feat: new defs for op-services

--- core/types/taiko_transaction.go
+++ core/types/taiko_transaction.go
@@ -1,34 +1,5 @@
 package types
 
-import (
-	"math/big"
-
-	"github.com/ethereum/go-ethereum/common"
-)
-
-// To make taiko-geth compatible with op-service.
-const DepositTxType = 0x7E
-
-// To make taiko-geth compatible with op-service.
-type DepositTx struct {
-	// SourceHash uniquely identifies the source of the deposit
-	SourceHash common.Hash
-	// From is exposed through the types.Signer, not through TxData
-	From common.Address
-	// nil means contract creation
-	To *common.Address `rlp:"nil"`
-	// Mint is minted on L2, locked on L1, nil if no minting.
-	Mint *big.Int `rlp:"nil"`
-	// Value is transferred from L2 balance, executed after Mint (if any)
-	Value *big.Int
-	// gas limit
-	Gas uint64
-	// Field indicating if this transaction is exempt from the L2 gas limit.
-	IsSystemTransaction bool
-	// Normal Tx data
-	Data []byte
-}
-
 func (tx *Transaction) MarkAsAnchor() error {
 	return tx.inner.markAsAnchor()
 }
