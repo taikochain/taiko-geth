b77a10f47482b141e36a3b152539af9f43d5308c
David <david@taiko.xyz>
2025-01-23 22:38:47 +0800

    feat: new defs for op-services

--- params/taiko_config.go
+++ params/taiko_config.go
@@ -1,6 +1,7 @@
 package params
 
 import (
+	"encoding/binary"
 	"math/big"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -8,10 +9,24 @@ import (
 
 // To make taiko-geth compatible with the op-service library, we need to define the following constants and functions.
 type ProtocolVersion [32]byte
+type ProtocolVersionComparison int
+
+func (p ProtocolVersion) Compare(other ProtocolVersion) (cmp ProtocolVersionComparison) {
+	return 0
+}
 
 type ProtocolVersionV0 struct {
 	Build                           [8]byte
 	Major, Minor, Patch, PreRelease uint32
+}
+
+func (v ProtocolVersionV0) Encode() (out ProtocolVersion) {
+	copy(out[8:16], v.Build[:])
+	binary.BigEndian.PutUint32(out[16:20], v.Major)
+	binary.BigEndian.PutUint32(out[20:24], v.Minor)
+	binary.BigEndian.PutUint32(out[24:28], v.Patch)
+	binary.BigEndian.PutUint32(out[28:32], v.PreRelease)
+	return
 }
 
 func u64(val uint64) *uint64 { return &val }
