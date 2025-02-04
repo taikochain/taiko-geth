54ca2198a63b680a53f8933c3a143cb2c14cff66
David <david@taiko.xyz>
2025-02-04 16:46:57 +0800

    feat: update api backend

--- ethclient/taiko_api.go
+++ ethclient/taiko_api.go
@@ -20,8 +20,8 @@ func (ec *Client) HeadL1Origin(ctx context.Context) (*rawdb.L1Origin, error) {
 }
 
 // WriteHeadL1Origin sets the latest L2 block's corresponding L1 origin.
-func (ec *Client) WriteHeadL1Origin(ctx context.Context, blockID *big.Int) (*rawdb.L1Origin, error) {
-	var res *rawdb.L1Origin
+func (ec *Client) WriteHeadL1Origin(ctx context.Context, blockID *big.Int) (*big.Int, error) {
+	var res *big.Int
 
 	if err := ec.c.CallContext(ctx, &res, "taiko_setHeadL1Origin", blockID); err != nil {
 		return nil, err
